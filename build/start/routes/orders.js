77509839f7de616593b37b10150be2f08c87736e
FelipeO16 <85635331+FelipeO16@users.noreply.github.com>
2023-05-11 09:47:04 -0300

    Update 11.05

--- build/start/routes/orders.js
+++ build/start/routes/orders.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
-Route_1.default.post('/api/order', 'Orders/Order.store').middleware('auth');
-Route_1.default.get('/api/orders', 'Orders/Order.index').middleware('auth');
-Route_1.default.put('/api/order', 'Orders/Order.update').middleware('auth');
-Route_1.default.get('/api/order/:id', 'Orders/Order.show').middleware('auth');
-Route_1.default.delete('/api/order', 'Orders/Order.destroy').middleware('auth');
+Route_1.default.post('/api/order', 'Orders/Order.store');
+Route_1.default.get('/api/orders', 'Orders/Order.index');
+Route_1.default.put('/api/order', 'Orders/Order.update');
+Route_1.default.get('/api/order/:id', 'Orders/Order.show');
+Route_1.default.delete('/api/order', 'Orders/Order.destroy');
 //# sourceMappingURL=orders.js.map
--- start/routes/orders.ts
+++ start/routes/orders.ts
@@ -1,7 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('/api/order', 'Orders/Order.store').middleware('auth')
-Route.get('/api/orders', 'Orders/Order.index').middleware('auth')
-Route.put('/api/order', 'Orders/Order.update').middleware('auth')
-Route.get('/api/order/:id', 'Orders/Order.show').middleware('auth')
-Route.delete('/api/order', 'Orders/Order.destroy').middleware('auth')
+Route.post('/api/order', 'Orders/Order.store')
+Route.get('/api/orders', 'Orders/Order.index')
+Route.put('/api/order', 'Orders/Order.update')
+Route.get('/api/order/:id', 'Orders/Order.show')
+Route.delete('/api/order', 'Orders/Order.destroy')
